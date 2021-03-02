1c52a1d76bdb5d8a55c9cdb770012b582d7b01fd
ruff samurai <qadree01@hotmail.com>
2021-03-02 09:33:02 -0600

    updated closures.js

--- closures.js
+++ closures.js
@@ -23,13 +23,13 @@ function outer() {
 */
   
 // Code Here
-
+const inner = outer()
 
 
 //Once you do that, invoke inner.
 
 //Code Here
-
+inner()
 
 
 ////////// PROBLEM 2 //////////
@@ -52,7 +52,7 @@ function callFriend(name) {
 */
 
 //Code Here
-
+let callJake = callFriend('Jake', 435-555-9248)
 
 
 ////////// PROBLEM 3 //////////
@@ -62,15 +62,25 @@ function callFriend(name) {
 */
 
 //Code Here
+function makeCounter(){
+  let count = 0
+
+  function addOne(){
+    return ++count
+  }
+
+  return addOne
+}
 
 
 
-//Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
+
+// Uncomment this once you make your function
+  // var count = makeCounter();
+  // count(); // 1
+  // count(); // 2
+  // count(); // 3
+  // count(); // 4
 
 
 
@@ -85,20 +95,25 @@ function callFriend(name) {
   http://stackoverflow.com/questions/17776940/javascript-module-pattern-with-example?answertab=votes#tab-top
 */
 
-function counterFactory(value) {
+function counterFactory() {
   // Code here.
+  let counter = 5;
 
   return {
-
-  };
+    inc: function(){
+      return ++counter;
+    },
+    dec: function(){
+      return --counter;
+    }
+  }
 }
 
 counter = counterFactory(10);
-// counter.inc() // 11
-// counter.inc() // 12
-// counter.inc() // 13
-// counter.dec() // 12
-
+counter.inc() // 11
+counter.inc() // 12
+counter.inc() // 13
+counter.dec() // 12
 
 
 ////////// PROBLEM 5 //////////
@@ -113,9 +128,12 @@ function motivation( firstname, lastname ) {
   var welcomeText = "You're doing awesome, keep it up";
 
   // code message function here.
+  function message(){
+    return `${welcomeText} ${firstname} ${lastname}.`
+  }
 
   //Uncommment this to return the value of your message function
-  //return message;
+  return message;
 }
 
 var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
@@ -144,8 +162,10 @@ var module = (function() {
   // outside our lexical scope
   return {
     // Code here.
+    
   };
-})();
+})
+module.publicMethod();
 
 
 
@@ -163,6 +183,7 @@ function secretNumber() {
 
   return {
     // Code here
+
   };
 }
 
